ec6744049b52ff8a4bcc12037a4fecb9f8d88f04
Belin Fieldson <thebelin@gmail.com>
2016-10-14 22:42:03 -0600

    Update upload procedure

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ var path = require('path'),
 
 // Open http server
   port_http = (process.env.PORT_HTTP == undefined ? env.PORT_HTTP : process.env.PORT_HTTP),
-  
+
+// Public URL
+  public_url = (process.env.PUBLIC_URL == undefined ? env.PUBLIC_URL : process.env.PUBLIC_URL),
+
 // Express framework
   express = require('express'),
 
@@ -41,6 +44,12 @@ var path = require('path'),
 // mkdirp allows us to make a folder
   mkdirp = require('mkdirp'),
 
+// For resizing images
+  resizeImg = require('resize-img'),
+
+// For reading the size of images
+  reader = require('image-size-reader'),
+
 // The metadata we are saving is held here
   metadataStorage = path.join(__dirname, 'data', 'uploads.json'),
 
@@ -59,7 +68,7 @@ var path = require('path'),
 
     // store the images in an array
     // each item in the array should be a type Image
-    this.images = images instanceof Array ? images : [];
+    this.files = images instanceof Array ? images : [];
 
     // the gallery has an identifying name, which also acts as security
     this.id = randomLorem({length: 8, syllables: 3});
@@ -69,25 +78,50 @@ var path = require('path'),
     }
   },
 
+// The metadata fetch middleware
+  metadataGet = function (req, res) {
+    // The item to get
+    var metaId = req.params.metaid;
+    if (metadata[metaId]) {
+      return res.json(metadata[metaId]);
+    }
+    return res.json({err:"Not Found"});
+  },
+
 // The File uploader middleware
   uploader = function (req, res) {
     var form = new multiparty.Form();
     form.parse(req, function (err, fields, files) {
       // Create a new gallery which serves the files which were uploaded
-      var gallery = new Gallery();
+      var gallery = new Gallery(),
+
+      // retain the gallery id
+        thisId = gallery.id;
 
       // Create an array of image objects from the files array
-      gallery.images = (function () {
+      gallery.files = (function () {
         return files.images.map(function (file) {
+          // These are the file measurements
+          var width = 0, height = 0,
           // Move the uploaded files into a subdirectory of the content folder
           // according to the galleryId
-          var newLocation = path.join(__dirname, 'content', gallery.id);
+            newLocation = path.join(__dirname, 'content', gallery.id);
 
           // Create the new folder
           mkdirp(newLocation);
 
           // move the file from the path attribute location
           // to the new location it will serve from
+          // @todo and resize it in the process
+          // resizeImg(fs.readFileSync(file.path), {width: 2048, height: 128}).then(buf => {
+          //   fs.writeFileSync('unicorn-128x128.png', buf);
+          // });
+          // reader(file.path).then((result) => {
+          //   console.log(result); // [width, height]
+          //   width = result[0] || 0;
+          //   height = result[1] || 0;
+          // });
+
           fs.rename(file.path, path.join(newLocation, file.originalFilename), function (err) {
             if (err) {
               console.log("Err: ", err);
@@ -95,24 +129,26 @@ var path = require('path'),
           });
 
           // @todo get uploaded image dimensions
-          // @todo resize uploaded image to conserve bandwidth
           return new Image({
             pictureName: file.originalFilename,
             artist: file.originalFilename,
-            url: path.join("content", gallery.id, file.originalFilename),
-            width: 0,
-            height: 0
+            url: 'http://' + path.join(public_url, "content", gallery.id, file.originalFilename),
+            width: width,
+            height: height
           });
         });
       }());
 
       // Save the gallery in the metadata
-      metadata[gallery.id] = gallery;
+      metadata[thisId] = gallery;
+
+      // Remove the internal id attribute
+      delete(metadata[thisId].id);
 
       // Save the metadata in the metadata.json file
       fs.writeFile(metadataStorage, JSON.stringify(metadata, null, 2));
 
-      res.json({fields: fields, files: files, gallery: gallery});
+      res.json({id: thisId});
     });
   };
 // End Variable declaration
@@ -129,6 +165,9 @@ app.use('/upload', uploader);
 // @todo compress content
 // Make the images which have been uploaded available
 app.use('/content', express.static(content_folder));
+
+// make the metadata available
+app.use('/json/:metaid', metadataGet);
 
 // Start the HTTP listener on http
 console.log('starting http server on port ' + port_http);
